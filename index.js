9ba49ca0401ddf7b7c55a843ee8affeeeea9c8d0
mungloli <munglolicon2002@gmail.com>
2023-12-08 20:46:03 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+
+async function connect() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/k4_nodejs_dev");
+    console.log("Connect successfully!!!");
+  } catch (error) {
+    console.log("Connect failure!!!");
+  }
+}
+
+connect();
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
